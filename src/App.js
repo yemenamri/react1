a8ac87487d6ccf81c20d17d7917b02399dc3980d
yemenamri <ahmedtique@gmail.com>
2021-07-28 19:57:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
+import React, { Component } from 'react';
+import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="col-md-6 offset-3">
+       <div>
+        <title>Form</title>
+        <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css" />
+        <div className="content">
+          <div className="row">
+            <div className="col-md-6 offset-3">
+              <form>
+                <div className="form-group">
+                  <label htmlFor="nom">Nom:</label>
+                  <input type="text" className="form-control" id="nom" name="nom" placeholder="Nom" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="Prenom">Prénom:</label>
+                  <input type="text" className="form-control" id="prenom" name="prenom" placeholder="Prénom" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="Sexe">Sexe:</label>
+                  <select className="form-control">
+                    <option value="Féminin">Féminin</option>
+                    <option value="Masculin">Masculin</option>
+                  </select>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email:</label>
+                  <input type="email" className="form-control" id="email" name="email" placeholder="Email" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="motdepasse">Mot de Passe:</label>
+                  <input type="password" className="form-control" id="motdepasse" name="motdepasse" placeholder="Mot de passe" />
+                </div>
+                <div className="form-check">
+                  <label className="form-chek-label">
+                    <input type="checkbox" className="form-check-input" />
+                    J'accepte les conditions
+                  </label>
+                </div>
+                <button type="submit" className="btn btn-primary">Envoyer</button>
+              </form></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
